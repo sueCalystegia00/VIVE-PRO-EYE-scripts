df48dd4779d437fa0cbad370c7f0a60c6fb09d1b
sue <52482280+susanbasket0410@users.noreply.github.com>
2020-07-02 14:42:25 +0900

    update CSVWriter
    テストしたところうまく動作はしましたが、若干ラグが発生するため一部のデータ出力に限ったほうがいいと思います

--- CSVWriter.cs
+++ CSVWriter.cs
@@ -8,41 +8,39 @@ using System.IO;
 namespace ViveSR.anipal.Eye
 {
 
-  ///<summary>
-  /// This source code is based on SRanipal_Unity_SDK version 1.1.0.1.
-  /// This class collects eye movement data measured by VIVE-PRO-EYE.
-  ///</summary>
+    ///<summary>
+    /// This source code is based on SRanipal_Unity_SDK version 1.1.0.1.
+    /// This class collects eye movement data measured by VIVE-PRO-EYE.
+    ///</summary>
 
-  public class CSVWriter : MonoBehaviour
-  {
-    GetEyeDataModule Module;
-    static StreamWriter streamwriter;
-    private string filepath = Application.dataPath + @"\Resorces\SaveData.csv";
-    private string dataLabels = "timestamp" +","+ "pupilDia_L" +","+ "pupilDia_R" +","+ 
-                                                  "pupilPosL.X" +","+ "pupilPosL.Y" +","+ 
-                                                  "pupilPosR.X" + "," + "pupilPosR.Y" +"," +
-                                                  "gazeDir_L.X" + "," + "gazeDir_L.Y" +"," + "gazeDir_L.Z" +","+
-                                                  "gazeDir_R.X" + "," + "gazeDir_R.Y" + "," + "gazeDir_R.Z";
-    /// <summary>
-    /// Use this for initialization
-    /// </summary>
-    void Start()
+    public class CSVWriter : MonoBehaviour
     {
-      streamwriter = new StreamWriter(filepath, true) { AutoFlush = true };
-      streamwriter.WriteLine(dataLabels);
+        private static StreamWriter streamwriter;
+        private string filepath;
+        private string dataLabels = "timestamp" + "," + "pupilDia_L" + "," + "pupilDia_R" + "," +
+                                                      "pupilPosL.X" + "," + "pupilPosL.Y" + "," +
+                                                      "pupilPosR.X" + "," + "pupilPosR.Y" + "," +
+                                                      "gazeDir_L.X" + "," + "gazeDir_L.Y" + "," + "gazeDir_L.Z" + "," +
+                                                      "gazeDir_R.X" + "," + "gazeDir_R.Y" + "," + "gazeDir_R.Z";
+        /// <summary>
+        /// Use this for initialization
+        /// </summary>
+        void Start()
+        {
+            filepath = Application.dataPath + "/Resorces/SaveData.csv";
+            streamwriter = new StreamWriter(filepath, true) { AutoFlush = true };
+            streamwriter.WriteLine(dataLabels);
+        }
 
-      Module = GetComponent<GetEyeDataModule>();
+        // Called by GetEyeDataModule
+        public static void Write()
+        {
+            streamwriter.WriteLine(GetEyeDataModule.timeStamp + "," + GetEyeDataModule.eyeOpenLeft + "," + GetEyeDataModule.eyeOpenRight + ","+
+                                               GetEyeDataModule.pupilDiameterLeft + "," + GetEyeDataModule.pupilDiameterRight + "," +
+                                               GetEyeDataModule.pupilPositionLeft.x + "," + GetEyeDataModule.pupilPositionLeft.y + "," +
+                                               GetEyeDataModule.pupilPositionRight.x + "," + GetEyeDataModule.pupilPositionRight.y + "," +
+                                               GetEyeDataModule.gaze_direction_Left.x + "," + GetEyeDataModule.gaze_direction_Left.y + "," + GetEyeDataModule.gaze_direction_Left.z + "," +
+                                               GetEyeDataModule.gaze_direction_Right.x + "," + GetEyeDataModule.gaze_direction_Right.y + "," + GetEyeDataModule.gaze_direction_Right.z);
+        }
     }
-
-    // Called by GetEyeDataModule
-    void Write()
-    {
-      writer90.WriteLine(timeStamp +","+ Module.eyeOpenLeft +","+ Module.eyeOpenRight +","+
-                                         Module.pupilDiameterLeft +","+ Module.pupilDiameterRight +","+
-                                         Module.pupilPositionLeft.x +","+ Module.pupilPositionLeft.y +","+ 
-                                         Module.pupilPositionRight.x +","+ Module.pupilPositionRight.y +","+
-                                         Module.gaze_direction_Left.x +","+ Module.gaze_direction_Left.y +","+ Module.gaze_direction_Left.z +","+
-                                         Module.gaze_direction_Right.x +","+ Module.gaze_direction_Right.y +","+ Module.gaze_direction_Right.z );
-    }
-  }
 }
